25bfadcd4eaf87476ccfccc6f61b0f0809e747f0
miraj <miraj00@gmail.com>
2021-08-24 12:41:30 -0400

    qustionairre for multiple entry functioning

--- app.js
+++ app.js
@@ -5,173 +5,172 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 const Employee = require("./lib/Employee");
-inquirer.registerPrompt('recursive', require('inquirer-recursive'));
+inquirer.registerPrompt("recursive", require("inquirer-recursive"));
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-
 const employees1 = [];
 const employees = [];
-// let repeat = true;
-// -------------
 
-//------------
-const prompt1 = [    
-   
- {
-    type: 'text',
-    name: 'name',
-    message: 'What is team member name?',
- },
- {
-    type: 'list',
-    name: 'role',
-    message: 'What is role of this team member?',
-    choices : ['Manager', 'Engineer', 'Intern']    
- }, 
- {
-    type: 'input',
-    name: 'officeNumber',
-    message: 'What is your office number',
-    when: function( answers ) {
+const prompt1 = [
+  {
+    type: "confirm",
+    name: "repeat",
+    message: "Do you want to add employee ?",
+  },
+];
+
+const prompt2 = [
+  {
+    type: "text",
+    name: "name",
+    message: "What is team member name?",
+    validate: nameText => {
+      if(nameText){
+          return true;
+      } else {
+          console.log('Please Enter Team Member Name');
+          return false;
+      }
+  }
+  },
+  {
+    type: "list",
+    name: "role",
+    message: "What is role of this team member?",
+    choices: ["Manager", "Engineer", "Intern"],
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "What is your office number",
+    when: function (answers) {
       // Only run if user answered Manager to the role prompt
       return answers.role === "Manager";
-        },
- },
- {
-    type: 'input',
-    name: 'github',
-    message: 'What is your GitHub username?',
-    when: function( answers ) {
+    },
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?",
+    when: function (answers) {
       // Only run if user answered Engineer to the role prompt
       return answers.role === "Engineer";
-        },
- }, 
- {
-    type: 'input',
-    name: 'school',
-    message: 'Which school do you go to?',
-    when: function( answers ) {
+    },
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "Which school do you go to?",
+    when: function (answers) {
       // Only run if user answered Intern to the role prompt
       return answers.role === "Intern";
-        },
- },              
-{
-    type: 'input',
-    name: 'email',
-    message: 'Please Enter member E-mail address :',
-},
-{
-    type: 'input',
-    name: 'id',
-    message: 'Please Enter ID of the team member :',
-},
-    {
-      type: "confirm",
-      name: "repeat",
-      message: "Do you want to add another employee ?",
     },
-]  
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Please Enter member E-mail address :",
+    validate: emailText => {
+      if(emailText){
+          return true;
+      } else {
+          console.log('Please Enter email of the member, if no Email type "NA"');
+          return false;
+      }
+  }
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "Please Enter ID of the team member :",
+    validate: (idInput) => {
+      // to make sure its a Number and no letters
+      if (isNaN(idInput)) {
+        console.log("Please Type ID Number (No letters allowed)");
+        return false;
+      } 
+      else {
+        return true;
+      }
+    },
 
-//--------------------------------
+  },
+];
 
-// function someFunction(){
-//     inquirer.prompt([
-//     {
-//           type: "confirm",
-//           name: "repeat",
-//           message: "Do you want to add another employee ?",
-//         },
-//     ]).then(
-//       if (answer.repeat === yes){
-//         someFunction() 
-//     }
-//     )
-//     }
+var firstPrompt = () => {
+  inquirer.prompt(prompt1).then((answer) => {
+ //   console.log(answer);
+    if (answer.repeat === true) {
+      secondPrompt();
+      } 
+      else {
+       console.log("Please check  output folder's team.html for generated HTML file") 
+      fs.writeFile(outputPath, render(employees), function(error, results){
+        if(error) console.log(error);
+       })
+         
+    }
+  });
+};
 
-//-----------------------------
- 
-// repeat = (
-//   await inquirer.prompt([
-//     {
-//       type: "confirm",
-//       name: "repeat",
-//       message: "Do you want to add another employee ?",
-//     },
-//   ])
-// ).repeat 
-//  while (repeat);
+firstPrompt();
 
+var secondPrompt = () => {
+  inquirer.prompt(prompt2).then((answers) => {
+//    console.log(answers);
 
+    const answerArr1 = new Employee(
+      answers.name,
+      answers.role,
+      answers.email,
+      answers.id
+    );
+    employees1.push(answerArr1);
+//    console.log(employees1);
 
-inquirer.prompt(prompt1)
-    .then((answers) => {
-        console.log(answers);
-  
-   
-        const answerArr1 = new Employee(answers.name, answers.role, answers.email, answers.id);
-        employees1.push(answerArr1);
-        console.log(employees1);
-        
-
-        if(answers.officeNumber){
-        const answerArr2 = new Manager(answers.name, answers.role, answers.email, answers.id, answers.officeNumber);
-        employees.push(answerArr2);
-        console.log(employees);
-//        inquirer.prompt(prompt1)
-        }
-        //engineeer works 
-        else if(answers.github){
-        const answerArr3 = new Engineer(answers.name, answers.role, answers.email, answers.id, answers.github);
-        employees.push(answerArr3);
-        console.log(employees);
-        prompt1;
-        }
-        // Intern works
-        else if (answers.school){
-        const answerArr4 = new Intern(answers.name, answers.role, answers.email, answers.id, answers.school);
-        employees.push(answerArr4);
-        console.log(employees);
-        prompt1;
-        }  
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-        fs.writeFile(outputPath, render(employees), function(error, results){
-            if(error) console.log(error);
-        })
-
-    })
-
-
-
-
-
-
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!
-//
+    if (answers.officeNumber) {
+      const answerArr2 = new Manager(
+        answers.name,
+        answers.role,
+        answers.email,
+        answers.id,
+        answers.officeNumber
+      );
+      employees.push(answerArr2);
+//    console.log(employees);
+      firstPrompt();    }
+    //engineeer works
+    else if (answers.github) {
+      const answerArr3 = new Engineer(
+        answers.name,
+        answers.role,
+        answers.email,
+        answers.id,
+        answers.github
+      );
+      employees.push(answerArr3);
+//    console.log(employees);
+      firstPrompt();
+    }
+    // Intern works
+    else if (answers.school) {
+      const answerArr4 = new Intern(
+        answers.name,
+        answers.role,
+        answers.email,
+        answers.id,
+        answers.school
+      );
+      employees.push(answerArr4);
+//    console.log(employees);
+      firstPrompt();
+    }
+  });
+};
